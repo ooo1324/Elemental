56ee7bb94cda58c079c4cb2b3ce24fc159f21ed5
ooo1324 <ooo1324@daum.net>
2023-08-19 03:05:53 +0900

    Feat : First Commit
    - 첫 커밋 (프로젝트 생성)
    - 메인 구성
    - 채연 씬 추가

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Fish : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+       
+        
+    }
+
+    private void OnMouseUp()
+    {
+     
+    }
+}
