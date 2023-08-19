61e88a8a5e22321e3e4832400b5dd1d77961d1d4
Jinu0717 <105360343+Jinu0717@users.noreply.github.com>
2023-08-19 13:56:41 +0900

    gage
    gage

--- Assets/Scripts/Air/Car_Move.cs
+++ Assets/Scripts/Air/Car_Move.cs
@@ -6,14 +6,29 @@ public class Car_Move : MonoBehaviour
 {
     public int Speed;
     public int Broke;
+    public int fire;
+
+    public GameObject[] Gas;
+    public Gage gage;
 
     void Start()
     {
-        
+        gage = GameObject.FindObjectOfType<Gage>();
+
+        if (Broke == 0)
+            fire = 1;
+        else 
+            fire = 2;
     }
 
     void Update()
     {
+        for (int i = 0; i < 6 * fire; i++)
+        {
+            Gas[i].transform.Translate(new Vector3(0, 0.01f, 0));
+            Gas[i].SetActive(true);
+        }
+
         if (transform.position.y > -6)
             transform.Translate(new Vector3(0, -Speed * Time.deltaTime, 0));
         else
@@ -23,9 +38,14 @@ public class Car_Move : MonoBehaviour
     private void OnMouseDown()
     {
         if (Broke == 0)
-            Debug.Log("정상");
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+            gage.Value[3] -= 15;
+        }
         else
-            Debug.Log("비정상");
+        {
+            gage.Value[3] += 10;
+        }
 
         GetComponent<BoxCollider2D>().enabled = false;
     }
