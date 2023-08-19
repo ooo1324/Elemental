3f73d9e088d2474bd42fc200a3f2583acf73d811
Cheyeon <scy011213@naver.com>
2023-08-20 00:19:37 +0900

    test
    test

--- Assets/Scripts/Timer_Slider.cs
+++ Assets/Scripts/Timer_Slider.cs
@@ -7,17 +7,29 @@ using UnityEngine.UI;
 
 public class Timer_Slider : MonoBehaviour
 {
+    private float cooltime = 0.0f;       //현재 남은 시간
 
-    public Slider progressbar;
+    private float cooltime_max = 300.0f;	//쿨타임
+
+    public Image disable;	//남은 시간을 표시할 이미지
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(CoolTimeFunc());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CoolTimeFunc()
     {
-        
+        while (cooltime < cooltime_max)
+        {
+            cooltime += Time.deltaTime;
+
+            //쿨타임 이미지
+            disable.fillAmount = cooltime / cooltime_max;
+
+
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
