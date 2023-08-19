e69889b948f6376f5f6cd27c4f9b5f35efc00d94
ooo1324 <ooo1324@daum.net>
2023-08-20 03:12:22 +0900

    최신

---
+++ Assets/Scripts/GaugeManager.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeManager : MonoBehaviour
+{
+    [SerializeField]
+    public List<ElementData> elementDataList;
+
+    public float delaySec;
+
+    private void Start()
+    {
+        for (int i = 0; i < elementDataList.Count; i++)
+        {
+            ElementData curdata = elementDataList[i];
+
+            curdata.barImg.fillAmount = curdata.value / 100;
+        }
+
+        StartCoroutine(GaugeManage());
+    }
+
+    IEnumerator GaugeManage()
+    {
+        while (true)
+        {
+            for (int i = 0; i < elementDataList.Count; i++)
+            {
+                ElementData curdata = elementDataList[i];
+
+                curdata.value -= curdata.reduce / 0.1f;
+
+                curdata.barImg.fillAmount = curdata.value / 100;
+
+                if (curdata.barImg.fillAmount <= 0)
+                {
+                    curdata.value = 0;
+                    //  Debug.Log("Game Over");
+                }
+                else if (curdata.barImg.fillAmount >= 1)
+                {
+                    curdata.value = 100;
+                }
+            }
+
+            yield return new WaitForSeconds(delaySec);
+        }  
+    }
+
+
+}
+
+[Serializable]
+public struct ElementData
+{
+
+    public Image barImg;
+
+    public float value;
+
+    public float reduce;
+}
+
+
