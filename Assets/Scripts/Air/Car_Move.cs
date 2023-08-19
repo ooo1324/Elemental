75d30313239e55314480647eed1f3304be46ce6d
Jinu0717 <105360343+Jinu0717@users.noreply.github.com>
2023-08-19 09:40:05 +0900

    Air
    공기

---
+++ Assets/Scripts/Air/Car_Move.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Move : MonoBehaviour
+{
+    public int Speed;
+    public int Broke;
+
+    void Update()
+    {
+        if (transform.position.y > -6.6f)
+            transform.Translate(new Vector3(0, -Speed * Time.deltaTime, 0));
+        else
+        {
+            transform.position = new Vector2(transform.position.x, 6.6f);
+            this.gameObject.SetActive(false);
+            this.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Broke == 0)
+            Debug.Log("정답");
+        else
+            Debug.Log("오답");
+
+        this.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
